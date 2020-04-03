149389f79a66563c4b73e80845e22453e2d459f5
eIyth <gkerno@gmail.com>
2020-04-04 01:00:00 +0200

    modif minime

--- commands/number.js
+++ commands/number.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "number",
     description: "donne des fatcs (random) sur des nombres",
     run: async (client, message, args) => {
-        if (args[1] === 'def') {
+        if (args[0] === 'def') {
             let getMot = async () => {
                 let response = await unirest.get("http://numbersapi.com/random/math")
                     .header("X-Mashape-Key", "e0d10c86dfmsh30f5845af8ce81ep148e9ajsn82de9e842fc6")
