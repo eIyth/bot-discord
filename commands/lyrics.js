c9958f4f1dc7c11e2754dd049d6d3fcb5f0c651e
eIyth <gkerno@gmail.com>
2020-04-04 17:40:03 +0200

    fix

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -8,7 +8,8 @@ module.exports = {
     usage: 'lyrics artist musique',
     run: async (client, message, args) => {
         let getLyrics = async () => {
-            var req = await unirest.get("https://genius.p.rapidapi.com/search/"+args[0])
+            var req = await unirest.get("https://genius.p.rapidapi.com/search/")
+            .query("q",args[0])
             .header("x-rapidapi-host", "genius.p.rapidapi.com")
             .header("x-rapidapi-key", "0878bdba20msh3a2cb883c1d7b48p153c9djsn1087d1e71043");
             let fact = req.body;
