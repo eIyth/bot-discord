5e78b784a27a1ff167b67ae8eecf25d53d20d6e7
eIyth <gkerno@gmail.com>
2020-04-04 18:49:39 +0200

    fix

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -7,25 +7,22 @@ module.exports = {
     description: "Renvoi les paroles d'une musique",
     usage: 'lyrics nom',
     run: async (client, message, args) => {
-        let getLyrics = async () => {
-            var req = unirest("GET", "https://genius.p.rapidapi.com/search/");
-            req.query({
-                "q": "humble"
-            });
-            req.headers({
-                "x-rapidapi-host": "genius.p.rapidapi.com",
-                "x-rapidapi-key": "0878bdba20msh3a2cb883c1d7b48p153c9djsn1087d1e71043"
-            });
+        var req = unirest("GET", "https://genius.p.rapidapi.com/search/");
+        req.query({
+            "q": "humble"
+        });
+        req.headers({
+            "x-rapidapi-host": "genius.p.rapidapi.com",
+            "x-rapidapi-key": "0878bdba20msh3a2cb883c1d7b48p153c9djsn1087d1e71043"
+        });
 
-            req.end(function (res) {
-                if (res.error) throw new Error(res.error);
-                let lyrics = res.body;
-                return lyrics;
-                console.log(res.body);
-            });
-            
-        }
-        let lyricsValue = getLyrics();
+        req.end(function (res) {
+            if (res.error) throw new Error(res.error);
+            let lyrics = res.body;
+            return lyrics;
+            console.log(res.body);
+        });
+        let lyricsValue = lyricsValue.response;
         console.log(lyricsValue.response.hits);
 
         const embed = new Discord.RichEmbed()
