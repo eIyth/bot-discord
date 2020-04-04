efb1260053a300f7ec952df5a64a4a8cbc32bf3f
eIyth <gkerno@gmail.com>
2020-04-04 17:43:42 +0200

    fix

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -9,13 +9,19 @@ module.exports = {
     run: async (client, message, args) => {
         let getLyrics = async () => {
             var req = await unirest.get("https://genius.p.rapidapi.com/search/")
-            .query("q",args[0])
-            .header("x-rapidapi-host", "genius.p.rapidapi.com")
-            .header("x-rapidapi-key", "0878bdba20msh3a2cb883c1d7b48p153c9djsn1087d1e71043");
+                .query("q", args[0])
+                .header("x-rapidapi-host", "genius.p.rapidapi.com")
+                .header("x-rapidapi-key", "0878bdba20msh3a2cb883c1d7b48p153c9djsn1087d1e71043");
             let fact = req.body;
             return fact;
         }
         let lyricsValue = await getLyrics();
         console.log(lyricsValue);
+
+        const embed = new Discord.RichEmbed()
+            .setColor('grey')
+            .setTitle("Paroles de" + lyricsValue.reponse.hits[0].result.title)
+            .addField('Blablabla', '');
+        message.channel.send(embed);
     }
 }
