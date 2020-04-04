1eb02262f5ce73565c7316dda81836cea8dcc831
eIyth <gkerno@gmail.com>
2020-04-04 22:01:27 +0200

    fix

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -32,11 +32,11 @@ module.exports = {
                     console.log(res);
                     const embed = new Discord.RichEmbed()
                     .setColor('#ffff00')
-                    .setTitle(response.hits[0].result.title)
-                    .setURL(response.hits[0].result.url)
-                    .setAuthor("par " + response.hits[0].result.primary_artist.name)
-                    .addField(response.hits[0].result.full_title, response.hits[0].result.url)
-                    .setThumbnail(response.hits[0].result.song_art_image_thumbnail_url)
+                    .setTitle("Test")
+                    .setURL("Test")
+                    .setAuthor("Test")
+                    .addField("TEST")
+                    .setThumbnail("TEST")
                     for (let i = 1; i < res.length; i++) {
                         embed.addField('Couplet '+ i, res[i])
                     }
