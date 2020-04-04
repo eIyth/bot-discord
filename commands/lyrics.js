6e1625fb4b13dea7b8551ae70e25c215673bc91f
eIyth <gkerno@gmail.com>
2020-04-04 19:51:58 +0200

    fix

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 var api = require('genius-api');
 var genius = new api("AY45__E_qL4cCaNaXoV1krPw-5TqqduhcKExOsmHDVMIJixlEQQeFAegYPsNrHhl");
-
+var JSSoup = require('jssoup').default;
 
 module.exports = {
     name: "lyrics",
@@ -14,10 +14,21 @@ module.exports = {
                 .setColor('#ffff00')
                 .setTitle(response.hits[0].result.title)
                 .setURL(response.hits[0].result.url)
-                .setAuthor("par " +response.hits[0].result.primary_artist.name)
-                .addField( response.hits[0].result.full_title, response.hits[0].result.url)
+                .setAuthor("par " + response.hits[0].result.primary_artist.name)
+                .addField(response.hits[0].result.full_title, response.hits[0].result.url)
                 .setThumbnail(response.hits[0].result.song_art_image_thumbnail_url);
             message.channel.send(embed);
+
+
+            path = connect_lyrics(song_id)
+
+            URL = response.hits[0].result.url;
+            page = requests.get(URL)
+
+            html = JSSoup(page.text, "html.parser");
+
+            lyrics = html.find("div", class_ = "lyrics").get_text();
+            console.log(lyrics);
         });
     }
 }
