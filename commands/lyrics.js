b95841b2d5172a25319d82185e46b55996d91f1b
eIyth <gkerno@gmail.com>
2020-04-04 16:59:59 +0200

    fix

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -7,13 +7,18 @@ module.exports = {
     description: "Renvoi les paroles d'une musique",
     usage : 'lyrics artist musique',
     run: async (client, message, args) => {
-        let getFact = async () => {
+        let getLyrics = async () => {
             let response = await axios.get("https://api.lyrics.ovh/v1/"+args[0]+"/"+args[1]);
             let fact = response.data;
             console.log(fact);
             return fact
         }
-        let factValue = await getFact();
-        message.channel.send(factValue.lyrics);
+        let factValue = await getLrycis();
+        if(factValue.lyrics,length>2000){
+            messaage.channel.send("desole le message est trop gros (>2000 caracteres)");
+        }
+        else {
+            message.channel.send(factValue.lyrics);
+        }
     }
 }
