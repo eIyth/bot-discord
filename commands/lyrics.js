f6ad4c3491932b90c3d3010a8f2129725ef56934
eIyth <gkerno@gmail.com>
2020-04-05 00:59:36 +0200

    test fetch

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
 var api = require('genius-api');
 var genius = new api("AY45__E_qL4cCaNaXoV1krPw-5TqqduhcKExOsmHDVMIJixlEQQeFAegYPsNrHhl");
-var request = require("request"),
-    fs = require('fs'),
-    extractor = require('unfluff');
+var cheerio = require("cheerio");
 
 module.exports = {
     name: "lyrics",
@@ -12,14 +10,38 @@ module.exports = {
     run: async (client, message, args) => {
         genius.search(JSON.stringify(args)).then(function (response) {
 
-            const embed = new Discord.RichEmbed()
-                .setColor('#ffff00')
-                .setTitle(response.hits[0].result.title)
-                .setURL(response.hits[0].result.url)
-                .setAuthor("par " + response.hits[0].result.primary_artist.name)
-                .addField(response.hits[0].result.full_title, response.hits[0].result.url)
-                .setThumbnail(response.hits[0].result.song_art_image_thumbnail_url);
-            message.channel.send(embed);
+            genius.prototype.getSongLyrics = async function getSongLyrics(geniusUrl) {
+                const response = await fetch(geniusUrl, {
+                    method: 'GET',
+                });
+                if (response.ok)
+                    return response.text();
+                throw new Error('Could not get song url ...');
+                const htmlText = undefined;
+                return parseSongHTML(htmlText);
+              };
+
+              function parseSongHTML(htmlText) {
+                const $ = cheerio.load(htmlText);
+                const lyrics = $('.lyrics').text();
+                const releaseDate = $('release-date .song_info-info').text();
+                return {
+                  lyrics,
+                  releaseDate,
+                };
+              };
+
+              console.log(getSongLyrics(response.hits[0].result.url));
+
+
+            // const embed = new Discord.RichEmbed()
+            //     .setColor('#ffff00')
+            //     .setTitle(response.hits[0].result.title)
+            //     .setURL(response.hits[0].result.url)
+            //     .setAuthor("par " + response.hits[0].result.primary_artist.name)
+            //     .addField(response.hits[0].result.full_title, response.hits[0].result.url)
+            //     .setThumbnail(response.hits[0].result.song_art_image_thumbnail_url);
+            // message.channel.send(embed);
             
             // URL = response.hits[0].result.url;
             // request({
