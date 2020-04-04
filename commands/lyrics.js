78b207cd8ee83a1d090d169352ab273359a8f35f
eIyth <gkerno@gmail.com>
2020-04-04 17:22:29 +0200

    genius api

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -5,28 +5,23 @@ const axios = require("axios");
 module.exports = {
     name: "lyrics",
     description: "Renvoi les paroles d'une musique",
-    usage : 'lyrics artist musique',
+    usage: 'lyrics artist musique',
     run: async (client, message, args) => {
         let getLyrics = async () => {
-            let response = await axios.get("https://api.lyrics.ovh/v1/"+args[0]+"/"+args[1]);
+
+
+            let reponse = await axios({
+                "method": "GET",
+                "url": "https://genius.p.rapidapi.com/search/"+args[0],
+                "headers": {
+                    "content-type": "application/octet-stream",
+                    "x-rapidapi-host": "genius.p.rapidapi.com",
+                    "x-rapidapi-key": "AY45__E_qL4cCaNaXoV1krPw-5TqqduhcKExOsmHDVMIJixlEQQeFAegYPsNrHhl"
+                }
+            });
+
             let fact = response.data;
             console.log(fact);
-            return fact
-        }
-        let lyricsValue = await getLyrics();
-        if(lyricsValue.lyrics.length>2000){
-            const embed = new Discord.RichEmbed()
-            .setColor('#0099ff')
-            .setTitle("Paroles de "+args[1])
-            .addField(args[1]+" par "+args[0], "Desole le message est trop long (>2000 caracteres)")
-            message.channel.send(embed);
-        }
-        else {
-            const embed = new Discord.RichEmbed()
-            .setColor('#0099ff')
-            .setTitle("Paroles de "+args[1])
-            .addField(args[1]+" par "+args[0], lyricsValue.lyrics)
-            message.channel.send(embed);
         }
     }
 }
