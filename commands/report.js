6f9386d230c507061a40aebd305003322bccb089
eIyth <gkerno@gmail.com>
2020-04-30 17:43:31 +0200

    added report

---
+++ commands/report.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "report",
+    description: "Envoyer un report sur un utilisateur",
+    usage : 'report @Username#0000 | report',
+
+    run: async (client, message, args) => {
+        if (args[0] === ''){
+            message.channel.send(`Vous devez choisir un utilisateur`);
+        }
+        else {
+            const embed = new Discord.RichEmbed()
+                .setColor('yellow')
+                .setTitle("Report envoyé sur "+ args[0])
+                .addField('Motif du report', args.toString());
+            message.channel.send(embed);
+        }
+    }
+}
